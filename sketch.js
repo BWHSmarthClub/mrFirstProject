7759cecae0a8a8a04592d9161af403471f4397e0
BWHSmarthClub <marthematicist@gmail.com>
2016-10-13 15:45:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ function draw() {
   //background( 200 , 200 , 255 , 10 );
   fill( 0 , 0 , 255 , 20 );
   
+  strokeWidth( 0.5 );
   stroke( 0 , 0 , 0 , 10 );
   line( 0 , 0 , mouseX , mouseY);
   line( 0 , windowHeight , mouseX , mouseY);
