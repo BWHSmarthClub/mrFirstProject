cd9a3bae81324f4cbcbe76ceba153b478929fc58
BWHSmarthClub <marthematicist@gmail.com>
2016-10-12 23:34:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas( 800 , 800 );
-  background( 200 , 200 , 255 );
+  //background( 200 , 200 , 255 );
 }
 
 function draw() {
-  fill( 0 , 0 , 255 , 20 );
-  stroke( 0 , 10 , 60 , 50 );
+  //fill( 0 , 0 , 255 , 20 );
+  //stroke( 0 , 10 , 60 , 50 );
   ellipse( mouseX , mouseY , 100 , 100 );
 }
